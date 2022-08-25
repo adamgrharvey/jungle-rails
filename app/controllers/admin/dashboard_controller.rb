ef02132ee7fc924e9773f5dfab346b7d7efe71c3
Adam <adamgrharvey@gmail.com>
2022-08-24 18:03:08 -0700

    adds DB counts for products and categories to the dashboard controller. adds these instance variables to the admin dashboard to display the current count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,9 +1,11 @@
 class Admin::DashboardController < ApplicationController
   include HttpAuthConcern
-  
+
   http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
                                password: ENV["HTTP_BASIC_PASSWORD"],
                                if: -> { ENV["HTTP_BASIC_PASSWORD"].present? }
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
